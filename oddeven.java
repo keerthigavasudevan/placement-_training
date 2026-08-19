7784207928053de24686062d48374001f187e321
keerthiga.v <keerthigavasudevan2007@gmail.com>
2026-08-19 09:04:00 +0530

    oddeven peoblem solved

---
+++ oddeven.java
@@ -0,0 +1,11 @@
+public class oddeven {
+    public static void main(String args[]){
+        int n=8;
+        if (n % 2 == 0) {
+            System.out.println(n + " is even");
+        } else {
+            System.out.println(n + " is odd");
+        }
+    }
+    
+}
